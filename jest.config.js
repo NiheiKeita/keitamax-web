5f77e195c685cecdb697c39ea8b2f0c7ee2e3732
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-07-21 09:25:53 +0900

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+        '^~/(.*)$': '<rootDir>/src/$1',
+    },
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+        '^.+\\.(js|jsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    },
+    globals: {
+        'ts-jest': {
+            tsconfig: {
+                jsx: 'react-jsx',
+            },
+        },
+    },
+    testMatch: [
+        "**/views/**/*.test.[jt]s?(x)"
+    ]
+}
